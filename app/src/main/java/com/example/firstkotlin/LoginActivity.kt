b193cbb92a8a04088d94682ed2928b571aa898b7
Syed Azam <azammasood2003@gmail.com>
2023-07-06 02:02:37 +0500

    UI

--- app/src/main/java/com/example/firstkotlin/LoginActivity.kt
+++ app/src/main/java/com/example/firstkotlin/LoginActivity.kt
@@ -1,18 +1,17 @@
 package com.example.firstkotlin
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
 class LoginActivity : AppCompatActivity()
 {
-//    private lateinit var inputUsername: EditText
-//    private lateinit var inputPassword: EditText
-//    private lateinit var btnLogin: Button
-
     override fun onCreate( savedInstanceState: Bundle? )
     {
         super.onCreate(savedInstanceState)
@@ -28,5 +27,25 @@ class LoginActivity : AppCompatActivity()
             val intentCamera = Intent(this, CameraActivity::class.java)
             startActivity(intentCamera)
         }
+
+        val forgotPasswordLink = findViewById<TextView>(R.id.forgotPassword)
+
+        forgotPasswordLink.setOnClickListener {
+            launchLearnerCloud(it)
+        }
+    }
+
+//    private fun launchLearnerCloud() {
+//        val forgotPasswordURL = "http://learnercloud.io/forgot-password"
+//
+//        val intentForgotPass  = Intent(Intent.ACTION_VIEW, Uri.parse(forgotPasswordURL))
+//        startActivity(intentForgotPass)
+//    }
+
+    fun launchLearnerCloud(view: View) {
+        val forgotPasswordURL = "http://learnercloud.io/forgot-password"
+
+        val intentForgotPass  = Intent(Intent.ACTION_VIEW, Uri.parse(forgotPasswordURL))
+        startActivity(intentForgotPass)
     }
 }
